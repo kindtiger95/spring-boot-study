31128b5ed7f219b5205dbd0f93d7277b2043089e
janginbeom-wsl2 <inbeom8742@naver.com>
2022-07-02 23:45:45 +0900

    fixed folder tree.

---
+++ my-first-spring/src/test/java/myfirstspring/MyFirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package myfirstspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
